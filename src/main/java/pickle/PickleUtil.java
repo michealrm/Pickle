7d88928a83185741584e51d648e5fa349abcf0bf
DESKTOP-4EH5F8F\jcgv6 <josecgomezvazquez@gmail.com>
2021-04-29 21:50:07 -0500

    Took out the 'if -5, print empty line' test

--- src/main/java/pickle/PickleUtil.java
+++ src/main/java/pickle/PickleUtil.java
@@ -2,8 +2,6 @@ package pickle;
 
 public class PickleUtil {
     public static boolean isInt(String tokenStr) {
-        if(tokenStr.equals("-5"))
-            System.out.println();
         boolean numeric = true;
         for(int i = 0; i < tokenStr.length(); i++) {
             char c = tokenStr.charAt(i);
